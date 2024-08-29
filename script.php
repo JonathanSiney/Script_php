50ab11b90c37725240d21137a75e5cef6d76e623
JonathanSiney <151563242+JonathanSiney@users.noreply.github.com>
2024-08-29 08:58:58 -0600

    script.php

--- script.php
+++ script.php
@@ -16,38 +16,50 @@ function getPDOConnection($host, $dbname, $username, $password) {
 }
 
 function insertarData($pdo, $tablaName) {
-   //php script.php nombre_tabla
     try {
-        $sql = "INSERT INTO $tablaName DEFAULT VALUES";
-        $stmt = $pdo->prepare($sql);
-        $stmt -> execute();
-        echo "Dato insertado con éxito en la tabla '$tablaName'.\n";
+        $data = [
+            ['columna1' => 'valor1', 'columna2' => 'valor2']
+        ];
+        foreach ($data as $row) {
+            $data = implode(",", array_keys($row));
+            $placeholders = implode(",", array_fill(0, count($row), '?'));
+            $sql = "INSERT INTO $tablaName ($data) VALUES ($placeholders)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(array_values($row));
+        }
+        echo "Datos insertados correctamente.\n";
     } catch (PDOException $e) {
         die("Error al insertar datos: " . $e->getMessage());
     }
 }
+
 function eliminarData($pdo, $tableName) {
-    //php script.php --dev rollback nombre_tabla
-    $sql = "DELETE FROM $tableName";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
+    try {
+        $sql = "DELETE FROM $tableName";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        echo "Datos eliminados con éxito en la tabla $tableName\n";
+    } catch (PDOException $e) {
+        die("Error al eliminar datos: " . $e->getMessage());
+    }
 }
+
 $options = [
     'dev' => isset($argv[1]) && $argv[1] == '--dev',
     'rollback' => isset($argv[2]) && $argv[2] == 'rollback',
     'table' => isset($argv[3]) ? $argv[3] : null,
 ];
+
 if ($options['dev'] && $options['rollback'] && $options['table']) {
     $tableName = $options['table'];
-    
-    echo "Datos eliminados con éxito en la tabla $tableName\n";
     $pdo = getPDOConnection($host, $dbname, $username, $password);
+    
     eliminarData($pdo, $tableName);
-}else{
+} else {
+   
     $tablaName = 'script_php'; 
     $pdo = getPDOConnection($host, $dbname, $username, $password);
     insertarData($pdo, $tablaName);
 }
 
 ?>
- 
